a21d7485297342945188a482c0393ed26d57ac05
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-10 17:25:26 +0100

    ciclato comic su main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,12 @@
 <div class="main_section">
     <div class="my_container">
         <div class="comics-wrapper">
-            <div class="single_comic">
-                @foreach ($comic as $comics)
-                    
-                @endforeach
-                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-                <span>TITOLO</span>
-            </div>
+            @foreach ($comics as $comic)
+                <div class="single_comic">  
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <span>{{ $comic['series'] }}</span>
+                </div>
+            @endforeach
           
 
         </div>
